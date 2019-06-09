7e07fb7878ce7d27ea5621153685286295c1e906
niwek <kewinli2@gmail.com>
2019-06-08 23:21:47 -0400

    Added a tic tac toe game :-)

---
+++ src/modules/Maze.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Maze extends React.Component {
+    render(){
+        return (
+            <div className="shopping-list">
+                <h1>My own shopping list</h1>
+                <ul>
+                    <li>Apple</li>
+                    <li>Bananas</li>
+                    <li>Oranges</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Maze;

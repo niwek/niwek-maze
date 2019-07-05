4f6e02bf3fb4cbf8a69e160ea01ff8c4f6cfb4b7
niwek <kewinli2@gmail.com>
2019-07-04 21:27:19 -0400

    Maze to one component and add scss

--- src/modules/Maze.js
+++ src/modules/Maze.js
@@ -1,14 +1,40 @@
 import React, { useState } from 'react';
-import Row from './Row';
+import './Maze.scss';
 
 function Maze() {
-  const [x] = useState(Math.floor(Math.random() * 5 + 5));
-  const [y] = useState(Math.floor(Math.random() * 5 + 5));
-  const [maze] = useState(Array(x).fill(Array(y).fill(null)));
+  const [range] = useState(5);
+  const [minimum] = useState(5);
+  const [width] = useState(Math.floor(Math.random() * range + minimum));
+  const [length] = useState(Math.floor(Math.random() * range + minimum));
+  const [maze] = useState(Array(width).fill(Array(length).fill(false)));
+  const [startingX] = useState(Math.floor(Math.random() * range + minimum));
+  const [startingY] = useState(Math.floor(Math.random() * range + minimum));
+
+  function handleCell(x, y, value) {
+    return (
+      <div className="square">
+        {value ? 'o' : 's'}
+      </div>
+    );
+  }
 
   function handleRow(rowNumber, row) {
-    return (<Row rowNumber={rowNumber} row={row} />);
+    return (
+      <div className="row">
+        {row.map((key, index) => handleCell(index, rowNumber, row[index]))}
+      </div>
+    );
   }
+
+  // function drawPath(xStart, yStart, xEnd, yEnd) {
+  //   // 0 = top, 1 = left, 2 = right, 3 = bottom
+  //   const randomDirection = Math.floor(Math.random() * 4);
+  //   if (randomDirection == 0) {
+
+  //   } else if (randomDirection == 1) {
+
+  //   }
+  // }
 
   return (
     <div className="shopping-list">
